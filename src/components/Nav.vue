f96a719ec44e98d621bed9ae3abeee2fb998320b
Yue2167 <12162981+yue2167@user.noreply.gitee.com>
2023-01-23 16:14:32 +0800

    添å加全局变量和全局样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -22,6 +22,7 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@import "~@/assets/style/helper.scss";
 nav {
   display: flex;
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
@@ -40,7 +41,7 @@ nav {
     }
   }
   > .item.selected{
-    color: #5EBA16;
+    color: $color-highlight;
   }
 }
 </style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,9 +1,56 @@
 <template>
-<div>
- <Layout>
-<p>Money</p>
- </Layout>
-</div>
+  <div>
+    <div>
+      <ul class="types">
+        <li class="selected">支出</li>
+        <li>收入</li>
+      </ul>
+    </div>
+    <Layout>
+      <div class="tags">
+        <ul class="current">
+          <li>餐饮</li>
+          <li>医疗</li>
+          <li>购物</li>
+          <li>交通</li>
+          <li>住宿</li>
+          <li>学习</li>
+          <li>娱乐</li>
+          <li>美妆</li>
+          <li>旅游</li>
+          <li>通讯</li>
+        </ul>
+      </div>
+      <div class="new">
+        <button>新增标签</button>
+      </div>
+      <div class="numberPad">
+        <div class="output">
+          <label class="notes">
+            <span>备注</span>
+            <input type="text">
+          </label>
+          <div class="number">100</div>
+        </div>
+        <div class="buttons">
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>删除</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>清空</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>OK</button>
+          <button>0</button>
+          <button>.</button>
+        </div>
+      </div>
+    </Layout>
+  </div>
 </template>
 
 <script lang="ts">
