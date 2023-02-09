64ecf9688eb907799cd4364e8b52e1f167d0498f
Yue2167 <12162981+yue2167@user.noreply.gitee.com>
2023-02-09 22:28:52 +0800

    使用js是è是è实现了一个组件â

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,23 +1,44 @@
 <template>
   <div>
     <ul class="types">
-      <li class="selected">支出</li>
-      <li>收入</li>
+      <li :class="type === '-' && 'selected'" @click="selectType('-')">支出
+      </li>
+      <li :class="type === '+' && 'selected'" @click="selectType('+')">收入
+      </li>
     </ul>
   </div>
 </template>
-<script lang="ts">
+<script lang="js">
 export default {
-  name: 'Types'
+  name: 'Types',
+  props: ['xxx'],
+  data() {
+    return {
+      type: '-'//'-'表示支出'+'表示收入
+    }
+  },
+  mounted() {
+    console.log(this.xxx);
+  },
+  methods: {
+    selectType(type) {//type只能是'-'和'+'中的一个
+      if (type !== '-' && type !== '+') {
+        throw new Error('type is unknown')
+      }
+      this.type = type
+    }
+  }
 };
 </script>
 <style scoped lang="scss">
 @import "~@/assets/style/helper.scss";
+
 .types {
   background: $color-highlight;
   display: flex;
   text-align: center;
   font-size: 20px;
+
   >li {
     width: 50%;
     height: 64px;
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <Layout class-prefix="layout">
-      <NumberPad/>
-      <Notes/>
-      <Tags/>
-      <Types/>
+      <NumberPad />
+      <Notes />
+      <Tags />
+      <Types xxx="hi" />
     </Layout>
   </div>
 </template>
@@ -15,7 +15,7 @@ import Tags from '@/components/Money/Tags.vue';
 import Types from '@/components/Money/Types.vue';
 export default {
   name: 'Money.vue',
-  components: {Types, Tags, Notes, NumberPad},
+  components: { Types, Tags, Notes, NumberPad },
 };
 </script>
 <style lang="scss">
@@ -25,4 +25,5 @@ export default {
 }
 </style>
 <style lang="scss" scoped>
+
 </style>
